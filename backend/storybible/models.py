e2b710a021dc440181e667814599ed61e5ca5ed4
Peps <pepepmjegano@gmail.com>
2026-06-29 16:31:44 +0800

    Fix duplicate UserProfile signal registration causing 500 on register

--- backend/storybible/models.py
+++ backend/storybible/models.py
@@ -1,7 +1,5 @@
 from django.db import models
 from django.conf import settings
-from django.db.models.signals import post_save
-from django.dispatch import receiver
 
 
 class Project(models.Model):
@@ -192,12 +190,6 @@ class UserProfile(models.Model):
 
     def __str__(self):
         return f'Profile for {self.user.username}'
-
-
-@receiver(post_save, sender=settings.AUTH_USER_MODEL)
-def create_user_profile(sender, instance, created, **kwargs):
-    if created:
-        UserProfile.objects.create(user=instance)
 
 
 class Conversation(models.Model):
